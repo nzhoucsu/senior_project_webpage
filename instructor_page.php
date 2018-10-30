bd6fe9d8bfe93105911e53a95615f4733d1d8a6f
nzhoucsu <nzhoucsu@gmail.com>
2018-10-30 17:47:19 -0400

    MODIFICATION: keep input text on web page after click Submit button

--- instructor_page.php
+++ instructor_page.php
@@ -18,11 +18,11 @@
 				<?php include('instructor_error_check.php');?>
 				<!-- Input user information -->
 				First Name<br>
-				<input type="text" name="fname" required><br>
+				<input type="text" name="fname" value="<?php echo $fname; ?>" required><br>
 				Last Name<br>
-				<input type="text" name="lname" required><br>
+				<input type="text" name="lname" value="<?php echo $lname; ?>" required><br>
 				CSU ID<br>
-				<input type="text" name="csuid" required><br>
+				<input type="text" name="csuid" value="<?php echo $csuid; ?>" required><br>
 				<!-- Input project operation -->
 				<h3>Project Operation</h3>
 				<input id="radio_add" type="radio" name="operation" value="add_pro" 
@@ -33,15 +33,18 @@
 				       onclick="del()"> Delet Project<br><br>
 				<div id="project_id" style="display: none">
 					Project #<br>  
-					<input type="text" name="pro_id"><br>
+					<input id="pro_id_input" type="text" name="pro_id"
+					value="<?php echo $pro_id; ?>"><br>
 				</div>
 				<div id="project_title"  style="display: none">
 					Title<br>
-					<input type="text" name="pro_title"><br>
+					<input id="pro_title_input" type="text" name="pro_title"
+					 value="<?php echo $pro_title; ?>"><br>
 				</div>
 				<div id="project_requirement"  style="display: none">
 					Requirement<br>
-					<input type="text" name="pro_requr"><br>
+					<input id="pro_requr_input" type="text" name="pro_requr"
+					 value="<?php echo $pro_requr; ?>"><br>
 				</div>
 				<script>
 					// This paragraph script for web page display when page refresh.
@@ -83,19 +86,28 @@
 	<script>
 		function add(){
 			sessionStorage.setItem("oprt_session_store", "radio_add_pro");
-			document.getElementById('project_id').style.display = 'none';
+			document.getElementById('pro_id_input').value    = '';	
+			document.getElementById('pro_title_input').value = '';			
+			document.getElementById('pro_requr_input').value = '';
+			document.getElementById('project_id').style.display    = 'none';
 			document.getElementById('project_title').style.display = 'block';
 			document.getElementById('project_requirement').style.display = 'block';
 		}
 		function modf(){
 			sessionStorage.setItem("oprt_session_store", "radio_mod_pro");
-			document.getElementById('project_id').style.display = 'block';
+			document.getElementById('pro_id_input').value    = '';	
+			document.getElementById('pro_title_input').value = '';			
+			document.getElementById('pro_requr_input').value = '';
+			document.getElementById('project_id').style.display    = 'block';
 			document.getElementById('project_title').style.display = 'block';
 			document.getElementById('project_requirement').style.display = 'block';
 		}
 		function del(){
 			sessionStorage.setItem("oprt_session_store", "radio_del_pro");
-			document.getElementById('project_id').style.display = 'block';
+			document.getElementById('pro_id_input').value    = '';	
+			document.getElementById('pro_title_input').value = '';			
+			document.getElementById('pro_requr_input').value = '';
+			document.getElementById('project_id').style.display    = 'block';
 			document.getElementById('project_title').style.display = 'none';
 			document.getElementById('project_requirement').style.display = 'none';
 		}		
