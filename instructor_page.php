293a043a95d69f7a13c672fba2f30ce01350473f
nzhoucsu <nzhoucsu@gmail.com>
2018-10-30 09:28:53 -0400

    modify right table size to 600fx

--- instructor_page.php
+++ instructor_page.php
@@ -11,16 +11,18 @@
 
 <body>
 
+
 	<div class="split_left">
 		<div>
-			<form class="relative">
+			<form class="relative" method="post" action="instructor_page.php">
+				<?php include('instructor_error_check.php');?>
 				<!-- Input user information -->
 				First Name<br>
-				<input type="text" name="fname" required><br>
+				<input type="text" name="fname" value="<?php echo $fname; ?>" required><br>
 				Last Name<br>
-				<input type="text" name="lname" required><br>
+				<input type="text" name="lname" value="<?php echo $lname; ?>" required><br>
 				CSU ID<br>
-				<input type="text" name="csuid" required><br>
+				<input type="text" name="csuid" value="<?php echo $csuid; ?>" required><br>
 				<!-- Input project operation -->
 				<h3>Project Operation</h3>
 				<input type="radio" name="operation" value="add_pro" checked
@@ -31,26 +33,30 @@
 				       onclick="del()"> Delet Project<br><br>
 				<div id="project_id" style="display: none;">
 					Project #<br>  
-					<input type="text" name="pro_id"><br>
+					<input type="text" name="pro_id" value="<?php echo $pro_id; ?>"><br>
 				</div>
 				<div id="project_title">
 					Title<br>
-					<input type="text" name="pro_title"><br>
+					<input type="text" name="pro_title" value="<?php echo $pro_title; ?>"><br>
 				</div>
 				<div id="project_requirement">
 					Requirement<br>
-					<input type="text" name="pro_requr"><br>
+					<input type="text" name="pro_requr" value="<?php echo $pro_requr; ?>"><br>
 				</div>
 				<div style="position: relative; top: 10px">
 					<input type="submit" name="bn_sbmt" value="Submit">
 				</div>					
 			</form>	
 		</div>			
-	</div>
+	</div> 
+
+
+
+	
 
 
 	<div class="split_right">
-		<div style="width: 98%; height: 700px; overflow: auto;">
+		<div style="width: 98%; height: 600px; overflow: auto;">
 			<?php include('import_project_list.php');?>
 		</div>
 	</div>
