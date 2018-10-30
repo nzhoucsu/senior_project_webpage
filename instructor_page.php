7f0d9ee5b7041fa9614acbcc141024e21279d9e4
nzhoucsu <nzhoucsu@gmail.com>
2018-10-30 17:16:44 -0400

    Add sessionStoriage.setItem to store radio button choice, further, to keep correct display when web page is updated

--- instructor_page.php
+++ instructor_page.php
@@ -18,41 +18,59 @@
 				<?php include('instructor_error_check.php');?>
 				<!-- Input user information -->
 				First Name<br>
-				<input type="text" name="fname" value="<?php echo $fname; ?>" required><br>
+				<input type="text" name="fname" required><br>
 				Last Name<br>
-				<input type="text" name="lname" value="<?php echo $lname; ?>" required><br>
+				<input type="text" name="lname" required><br>
 				CSU ID<br>
-				<input type="text" name="csuid" value="<?php echo $csuid; ?>" required><br>
+				<input type="text" name="csuid" required><br>
 				<!-- Input project operation -->
 				<h3>Project Operation</h3>
-				<input type="radio" name="operation" value="add_pro" checked
+				<input id="radio_add" type="radio" name="operation" value="add_pro" 
 				       onclick="add()"> Add Project<br>
-				<input type="radio" name="operation" value="mod_pro"
+				<input id="radio_mod" type="radio" name="operation" value="mod_pro" 
 				       onclick="modf()"> Modify Project<br>
-				<input type="radio" name="operation" value="del_pro"
+				<input id="radio_del" type="radio" name="operation" value="del_pro" 
 				       onclick="del()"> Delet Project<br><br>
-				<div id="project_id" style="display: none;">
+				<div id="project_id" style="display: none">
 					Project #<br>  
-					<input type="text" name="pro_id" value="<?php echo $pro_id; ?>"><br>
+					<input type="text" name="pro_id"><br>
 				</div>
-				<div id="project_title">
+				<div id="project_title"  style="display: none">
 					Title<br>
-					<input type="text" name="pro_title" value="<?php echo $pro_title; ?>"><br>
+					<input type="text" name="pro_title"><br>
 				</div>
-				<div id="project_requirement">
+				<div id="project_requirement"  style="display: none">
 					Requirement<br>
-					<input type="text" name="pro_requr" value="<?php echo $pro_requr; ?>"><br>
+					<input type="text" name="pro_requr"><br>
 				</div>
+				<script>
+					// This paragraph script for web page display when page refresh.
+					var choice = sessionStorage.getItem("oprt_session_store");
+					if(choice == 'radio_add_pro'){
+						document.getElementById('radio_add').checked = true;
+						document.getElementById('project_id').style.display = 'none';
+						document.getElementById('project_title').style.display = 'block';
+						document.getElementById('project_requirement').style.display = 'block';
+					}
+					else if (choice == 'radio_mod_pro') {
+						document.getElementById('radio_mod').checked = true;
+						document.getElementById('project_id').style.display = 'block';
+						document.getElementById('project_title').style.display = 'block';
+						document.getElementById('project_requirement').style.display = 'block';
+					}
+					else if (choice == 'radio_del_pro') {
+						document.getElementById('radio_del').checked = true;
+						document.getElementById('project_id').style.display = 'block';
+						document.getElementById('project_title').style.display = 'none';
+						document.getElementById('project_requirement').style.display = 'none';
+					}
+				</script>
 				<div style="position: relative; top: 10px">
 					<input type="submit" name="bn_sbmt" value="Submit">
 				</div>					
 			</form>	
 		</div>			
 	</div> 
-
-
-
-	
 
 
 	<div class="split_right">
@@ -64,21 +82,23 @@
 
 	<script>
 		function add(){
+			sessionStorage.setItem("oprt_session_store", "radio_add_pro");
 			document.getElementById('project_id').style.display = 'none';
 			document.getElementById('project_title').style.display = 'block';
 			document.getElementById('project_requirement').style.display = 'block';
 		}
 		function modf(){
+			sessionStorage.setItem("oprt_session_store", "radio_mod_pro");
 			document.getElementById('project_id').style.display = 'block';
 			document.getElementById('project_title').style.display = 'block';
 			document.getElementById('project_requirement').style.display = 'block';
 		}
 		function del(){
+			sessionStorage.setItem("oprt_session_store", "radio_del_pro");
 			document.getElementById('project_id').style.display = 'block';
 			document.getElementById('project_title').style.display = 'none';
 			document.getElementById('project_requirement').style.display = 'none';
-		}
-		
+		}		
 	</script>
 
 
