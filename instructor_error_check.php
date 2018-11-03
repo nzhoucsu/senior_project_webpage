d7610c2e0d3559fa64f65dddd625a991d006f463
nzhoucsu <nzhoucsu@gmail.com>
2018-11-03 17:26:01 -0400

    MODIFY: add_project

--- instructor_error_check.php
+++ instructor_error_check.php
@@ -11,7 +11,7 @@ define("VALID_SPONSOR",   3);
 define("INVALID_SPONSOR", 4);
 define("VALID_TITLE",     5);
 define("INVALID_TITLE",   6);
-define("SUCCESS_MODF",    0);
+define("NO_RADIO_BUTTON_CHOICE", 7);
 
 
 // Variables are to store user id info.
@@ -23,15 +23,53 @@ $pro_title = "";
 $pro_requr = "";
 $pro_id_array = "";
 $operation = "";
-// A variable is to store error information.
-$errors = array();
 
 
+// Display database connection notice for user.
+function notice_for_operation($r_val){
+	if($r_val == FAILED) {
+		echo "<font color='red'>";
+		echo "Failed!<br>";
+		echo "Fail to connect database.<br>";
+		echo "<br></font>";
+	}
+	elseif ($r_val == NO_RADIO_BUTTON_CHOICE) {
+		echo "<font color='red'>";
+		echo "Failed!<br>";
+		echo "Please select an operation.<br>";
+		echo "<br></font>";
+	}
+}
+
+
+// Display project addition notice for user.
+function notice_for_add($r_val){
+	if ($r_val == FAILED) {
+		echo "<font color='red'>";
+		echo "Failed!<br>";
+		echo "Fail to connect database.<br>";
+		echo "<br></font>";
+	}
+	elseif ($r_val == INVALID_TITLE) {
+		echo "<font color='red'>";
+		echo "Failed!<br>";
+		echo "Added project title is empty.<br>";
+		echo "<br></font>";
+	}
+	elseif ($r_val == SUCCESS) {
+		echo "<font color='blue'>";
+		echo "Successfully add a project!<br>";
+		echo "<br></font>";
+	}
+}
+
+
+// Display project modification notice to user.
 function notice_for_modf($r_val){
 	if ($r_val == INVALID_PROID) {
 		echo "<font color='red'>";
 		echo "Failed!<br>";
-		echo "Input project id is invailid.<br>";
+		echo "Modified project id is invailid.<br>";
 		echo "<br></font>";
 	}
 	elseif ($r_val == INVALID_SPONSOR) {
@@ -42,41 +80,39 @@ function notice_for_modf($r_val){
 	}elseif ($r_val == INVALID_TITLE) {
 		echo "<font color='red'>";
 		echo "Failed!<br>";
-		echo "Input project title is empty.<br>";
+		echo "Modified project title is empty.<br>";
 		echo "<br></font>";
 	}
 	elseif ($r_val == FAILED) {
 		echo "<font color='red'>";
 		echo "Failed!<br>";
-		echo "Errors display in DEBUG OUTPUT.<br>";
+		echo "Fail to connect database.<br>";
 		echo "<br></font>";
 	}
 	elseif ($r_val == SUCCESS) {
 		echo "<font color='blue'>";
-		echo "Modification Successful!<br>";
+		echo "Successfully modify a project!<br>";
 		echo "<br></font>";
 	}
 }
 
 
-function insert_new_project($db_conn, $pro_title, $pro_requr, $fname, $lname){
+function add_project($db_conn, $pro_title, $pro_requr, $fname, $lname){
 	global $errors;
 	$table_name = "project";
 	// Check new project title.
 	if(empty($pro_title)){
-		array_push($errors, "Project title is empty.");
-		return -1;
+		return INVALID_TITLE;
 	}
 	// Insert data.		
 	$sql = "INSERT INTO {$table_name} (title, requirement, fname, lname) 
 	        VALUES('$pro_title', '$pro_requr', '$fname', '$lname')";
 	$results = mysqli_query($db_conn, $sql);
 	if (!$results) {
-	    array_push($errors, "Failed to add project into database.");
-	    return -1;
+	    return FAILED;
 	}
 	// Return success.
-	return 0;
+	return SUCCESS;
 }
 
 
@@ -174,90 +210,7 @@ function update_project($db_conn, $pro_id, $pro_title, $pro_requr){
 	    array_push($errors, "Failed to update proinfo in TABLE $table_name.");
 	    return FAILED;
 	}
-	return SUCCESS_MODF;
-}
-
-
-function delete_project($db_conn, $fname, $lname, $del_pro_id){
-	global $errors, $pro_array;
-	$table_name = "project";
-	$table_name_2 = "preference";
-	$error_flag = 0;
-	// Check deleted project id.
-	if(empty($del_pro_id)){
-		array_push($errors, "Project ID is empty.");
-		return -1;
-	}
-	// Analyze deleted items.
-	$pro_array = explode(",", $del_pro_id);	
-	$a = array();
-	foreach ($pro_array as $pro_item) {
-		if(intval($pro_item) == 0){
-			array_push($errors, "Project ID must be integer(s) separated by comma ','.");
-			return -1;
-		}
-		else{
-			array_push($a, intval($pro_item));
-		}
-	}
-	// Acquire unique deleted items.
-	$a = array_unique($a);
-	foreach ($a as $item) {
-		// Check if project $item is in system.
-		$sql = "SELECT COUNT(*) AS count FROM {$table_name} WHERE pro_id=$item";
-		$results = mysqli_query($db_conn, $sql);
-		if (!$results) {
-			array_push($errors, "Failed to access deleted project {$item}.");
-			$error_flag = -1;
-			continue;
-		}
-		$row=mysqli_fetch_assoc($results);
-		if ($row["count"] == 0) { 
-			array_push($errors, "Failure! Project {$item} is not in this system.");
-			$error_flag = -1;
-			continue;
-		}
-		// Check if this project has been registered by students.
-		$sql = "SELECT COUNT(*) AS count FROM {$table_name_2} WHERE pro_id=$item";
-		$results = mysqli_query($db_conn, $sql);
-		if (!$results) {
-			array_push($errors, "Failed to access deleted project {$item}.");
-			$error_flag = -1;
-			continue;
-		}
-		$row=mysqli_fetch_assoc($results);
-		if ($row["count"] != 0) { 
-			array_push($errors, "Failure! Project {$item} has been enrolled by students.");
-			$error_flag = -1;
-			continue;
-		}
-		// Check if current user is the author created this project.
-		// Only project's author can delete the project.
-		$sql = "SELECT fname, lname FROM {$table_name} WHERE pro_id={$item}";
-		$results = mysqli_query($db_conn, $sql);
-		if (!$results) {
-			array_push($errors, "Failed to access author of deleted project {$item}.");
-			$error_flag = -1;
-			continue;
-		}
-		$row=mysqli_fetch_assoc($results);
-		$strcmp_1 = strcmp(strtolower($row["fname"]), strtolower($fname));
-		$strcmp_2 = strcmp(strtolower($row["lname"]), strtolower($lname));
-		if ($strcmp_1!=0 || $strcmp_2!=0){
-			array_push($errors, "Failure! You are not author of project {$item}.");
-			$error_flag = -1;
-			continue;
-		}
-		// Delete project $item.
-		$sql = "DELETE FROM {$table_name} WHERE pro_id={$item}";
-		$results = mysqli_query($db_conn, $sql);
-		if (!$results) {
-			array_push($errors, "Failure! Meet problems when to delete project {$item}.");
-			$error_flag = -1;
-			continue;
-		}
-	}
-	return $error_flag;	
+	return SUCCESS;
 }
 
 
@@ -265,8 +218,8 @@ if(isset($_POST['bn_sbmt'])){
 	// Connect database.
 	$db_conn = mysqli_connect('localhost', 'root', '0302', 'senior_project_db');
 	if (!$db_conn) {
-		array_push($errors, "Failed to connect database!");
-		goto error_report;
+		$r_val = FAILED;
+		goto notice_for_user;
 	}
 	
 	// Acquire user id info input.
@@ -275,16 +228,16 @@ if(isset($_POST['bn_sbmt'])){
 	
 	// Implement user's operation.
 	$operation = $_POST['operation'];
+	if (empty($operation)) {
+		$r_val = NO_RADIO_BUTTON_CHOICE;
+		goto notice_for_user;
+	}
 	switch ($operation) {
 		case 'add_pro':
 			$pro_title = $_POST['pro_title'];
 			$pro_requr = $_POST['pro_requr'];
 			// Insert new project information into database.
-			$r_val = insert_new_project($db_conn, $pro_title, $pro_requr, $fname, $lname);
-			if ($r_val == -1){
-				mysqli_close($db_conn);
-				goto error_report;
-			}
+			$r_val = add_project($db_conn, $pro_title, $pro_requr, $fname, $lname);
 			break;
 		case 'mod_pro':
 			$pro_id = $_POST['pro_id'];
@@ -294,45 +247,37 @@ if(isset($_POST['bn_sbmt'])){
 			$r_val = modify_project($db_conn, 
 									$pro_id, $pro_title, $pro_requr, 
 									$fname, $lname);	
-			if ($r_val == FAILED){
-				mysqli_close($db_conn);
-				goto error_report;
-			}
 			break;
-		case 'del_pro':
-			$pro_id = $_POST['pro_id'];
-			// Delete existing project(s).
-			$r_val = delete_project($db_conn, $fname, $lname, $pro_id);	
-			if ($r_val == -1){
-				mysqli_close($db_conn);
-				goto error_report;
-			}
-				break;
+		// case 'del_pro':
+		// 	$pro_id = $_POST['pro_id'];
+		// 	// Delete existing project(s).
+		// 	$r_val = delete_project($db_conn, $fname, $lname, $pro_id);	
+		// 	if ($r_val == -1){
+		// 		mysqli_close($db_conn);
+		// 		goto error_report;
+		// 	}
+		// 		break;
 		default:
 			break;
 	}
 
 	mysqli_close($db_conn);
 
-	error_report:
-	if(count($errors)>0){
-		echo "<br> DEBUG OUPUT<br>";
-		foreach ($errors as $error) {
-			echo $error;
-			echo "<br>";
-		}
-	}
 	// Display user notice information.
+	notice_for_user:
 	switch ($operation) {
+		case '':
+			notice_for_operation($r_val);
+			break;
 		case 'add_pro':
-			# code...
+			notice_for_add($r_val);
 			break;
 		case 'mod_pro':
 			notice_for_modf($r_val);
 			break;
-		case 'del_pro':
-			# code...
-			break;
+		// case 'del_pro':
+		// 	# code...
+		// 	break;
 		default:
 			# code...
 			break;
