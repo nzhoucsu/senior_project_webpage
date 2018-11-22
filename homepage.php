348e02108ad37f18c8dc7b3b6ebd856814bf2963
nzhoucsu <nzhoucsu@gmail.com>
2018-11-22 13:10:06 -0500

    CREATE

---
+++ homepage.php
@@ -0,0 +1,6 @@
+<div style="padding: 50px">
+<center><h2>Home Page</h2>
+<p><a href="http://localhost/student_page.php">Student Entrance</a></p> 
+<p><a href="http://localhost/instructor_page.php">Instructor Entrance</a></p>  
+<p><a href="http://localhost/admin_page.php">Admin Entrance</a></p></center>
+</div>
